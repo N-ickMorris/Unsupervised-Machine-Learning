a127ee5f89d8e8fb0f9a92b177d284c60c41640d
N-ickMorris <njm2868@g.rit.edu>
2018-12-08 14:53:08 -0500

    Update kmeans.R

--- kmeans.R
+++ kmeans.R
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------------
 
 # set the path of where the input files are
-mywd = "C:/Users/Nick Morris/Downloads/Data-Science/Model-Scripts/Unsupervised/K-Means"
+mywd = "C:/ ... /Unsupervised/K-Means"
 
 # create a name for a .txt file to log progress information while parallel processing
 myfile = "log.txt"
